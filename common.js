2f59c90077484726763a07f068d99828b5c07b4a
tetsu333 <70054989+tetsu333@users.noreply.github.com>
2024-01-08 19:32:53 +0900

    Create common.js

---
+++ common.js
@@ -0,0 +1,13 @@
+function looopAnimation(id, className, delay) {
+  var element = document.getElementById(id);
+  element.addEventListener("animationend", listener);
+  
+  function listener(event) {
+    event.target.classList.remove(className);
+    setTimeout(playAnimation, delay);
+  }
+
+  function playAnimation() {
+    element.classList.add(className);
+  }
+}
